05a7b8649b0a3a3665da8e14f62e2e98dbcc5f35
James Webb <james@ultra-horizon.com>
2018-05-08 01:42:22 +0100

    Updated ENV Variable

--- processing/verification.py
+++ processing/verification.py
@@ -1,8 +1,9 @@
 from classes.errors import Unauthorized
+import os
 
 # Crude pre-shared key verification method for leaderboard. Should be OAuth in future.
 def verify(challenge):
-    if challenge == "***REMOVED***":
+    if challenge == os.environ['PASSWORD']:
         return
     else:
         raise Unauthorized("Invalid pre-shared key")
